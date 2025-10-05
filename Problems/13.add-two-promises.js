c6d726c5f7c668eb073736c7501b0fb212c373ef
SheheryarAhsan <sheheryar.ahsan.dev@gmail.com>
2025-10-05 17:27:09 +0500

    13 and 14 Problem

---
+++ Problems/13.add-two-promises.js
@@ -0,0 +1,12 @@
+/* Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number.
+ The returned promise should resolve with the sum of the two numbers. */
+
+var addTwoPromises = async function (promise1, promise2) {
+    const [v1, v2] = await Promise.all([promise1, promise2]);
+    return v1 + v2;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
---
+++ Problems/14.sleep.js
@@ -0,0 +1,12 @@
+/* Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+Note that minor deviation from millis in the actual sleep duration is acceptable. */
+
+function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
